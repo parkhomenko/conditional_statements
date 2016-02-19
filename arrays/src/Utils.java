933d0af94605245b92b072fe38a215a59d19dbb7
parkhomenko <stanislav.parkhomenko@gmail.com>
2016-02-20 00:08:56 +0200

    Arrays and Strings

---
+++ arrays/src/Utils.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Utils {
+
+    public static int[] getArrayFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Size of array: ");
+        int size = scanner.nextInt();
+
+        System.out.println("Fill up the array: ");
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the " + i + " element: ");
+            array[i] = scanner.nextInt();
+        }
+
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        System.out.println("Array: ");
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1)
+                System.out.println(array[i]);
+            else
+                System.out.print(array[i] + ", ");
+        }
+    }
+}
